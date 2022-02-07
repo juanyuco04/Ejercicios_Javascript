0321422d113a93669e0de3b72b4f52eac4580764
jaun0819 <ambientey13@PITAPRTNNFSD195.FormacionSena.RED>
2022-02-07 15:40:51 -0500

    subiendo cambios

---
+++ js/1_Ejer_Javascript.js
@@ -0,0 +1,12 @@
+//console.log ("hola Mundo")
+
+// variable con nombre 
+
+let mombreUsuario = "Juan fernando"
+
+function mostrarNombreUsuario() {
+    console.log("seÑor(a)" + mombreUsuario)
+    console.log("¿como se encuentra?")
+}
+
+mostrarNombreUsuario();
