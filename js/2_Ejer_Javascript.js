fe01a5fa7bf1cfc0d59694aa433d55f3a778fb13
jaun0819 <ambientey13@PITAPRTNNFSD195.FormacionSena.RED>
2022-02-07 17:32:39 -0500

    ultimos cambios por hoy

--- js/2_Ejer_Javascript.js
+++ js/2_Ejer_Javascript.js
@@ -1,4 +1,6 @@
- let costoProducto = 55000 ;
+// ejercicio 3
+
+let costoProducto = 20000 ;
  let impuestoProducto = 0.1 ;
  let costoDomicilio = 2000;
 
@@ -7,6 +9,8 @@
 
  console.log("$" + costoTotal + " " + "costo total sin incluir el domicilio") ;
  console.log("$" + costoDomicilioTotal + " " + "costo total con domicilio incluido");
+
+ //ejercicio 4
 
  let descuentoMayor = 0.1 ;
  let descuentoMenor = 0.05 ;
@@ -20,7 +24,7 @@
        if(costoProducto >= 100000){
            console.log ("tendra un descuento del 10%, que equivale a un total de " + descuentoTotalMayor )
         }
-       else if (costoProducto >= 30000 || costoProducto < 100000){
+       else if (costoProducto >= 30000 && costoProducto < 100000){
            console.log("tendra un descuento del 5%; que equivale a un total de " + descuentoTotalMenor)
         }
        else  {
@@ -29,3 +33,4 @@
      }
 
    costoDescuento();
+   
